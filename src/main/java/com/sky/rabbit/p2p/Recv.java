e0f4f4ed0e8bcf26a2693e003e79ba45d74f944d
szw <shizhanwei123@aliyun.com>
2019-09-06 08:33:48 +0800

    点对点代码

--- src/main/java/com/sky/rabbit/p2p/Recv.java
+++ src/main/java/com/sky/rabbit/p2p/Recv.java
@@ -6,28 +6,34 @@ import java.io.IOException;
 
 public class Recv {
 
-	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_NAME = "queue-test-01";
 
 	public static void main(String[] argv) throws Exception {
+		// 1.创建一个连接工厂ConnectionFactory
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("192.168.77.10");
-		factory.setUsername("szw");
-		factory.setPassword("szw");
-		factory.setVirtualHost("/none");
+		factory.setHost("49.234.60.118");
+		factory.setUsername("sky-test-01");
+		factory.setPassword("sky-test-01");
+		factory.setVirtualHost("host-test-01");
+
+		// 2.通过连接工厂建立连接
 		Connection connection = factory.newConnection();
+
+		// 3.通过连接创建通道
 		Channel channel = connection.createChannel();
 
+		// 4.声明队列
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-		Consumer consumer = new DefaultConsumer(channel) {
+		// 5.创建消费者开启监听
+		channel.basicConsume(QUEUE_NAME, true, new DefaultConsumer(channel) {
 			@Override
 			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-					byte[] body) throws IOException {
+									   byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
 				System.out.println(" [x] Received '" + message + "'");
 			}
-		};
-		channel.basicConsume(QUEUE_NAME, true, consumer);
+		});
 	}
 }
--- src/main/java/com/sky/rabbit/p2p/Send.java
+++ src/main/java/com/sky/rabbit/p2p/Send.java
@@ -6,18 +6,23 @@ import com.rabbitmq.client.ConnectionFactory;
 
 public class Send {
 
-	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_NAME = "queue-test-01";
 
 	public static void main(String[] argv) throws Exception {
+		// 1.创建一个连接工厂ConnectionFactory
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("192.168.77.10");
-		factory.setUsername("szw");
-		factory.setPassword("szw");
-		factory.setVirtualHost("/none");
+		factory.setHost("49.234.60.118");
+		factory.setUsername("sky-test-01");
+		factory.setPassword("sky-test-01");
+		factory.setVirtualHost("host-test-01");
+
+		// 2.通过连接工厂建立连接
 		Connection connection = factory.newConnection();
+
+		// 3.通过连接创建通道
 		Channel channel = connection.createChannel();
 
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		// 4.发布消息
 		String message = "Hello World!111";
 		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
 		System.out.println(" [x] Sent '" + message + "'");
